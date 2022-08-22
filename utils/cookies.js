2ae03cc1ce3962e9e7723577b064d289540d9a13
irfnd <irfandiabimanyu@gmail.com>
2022-08-23 06:35:55 +0700

    [Update] - Fix minor

--- utils/cookies.js
+++ utils/cookies.js
@@ -2,8 +2,8 @@ import parse from "parse-duration";
 
 const cookiesOptions = {
 	maxAge: parse(process.env.NEXT_PUBLIC_COOKIE_EXPIRE) / 1000,
-	httpOnly: true,
-	sameSite: "lax",
+	domain: "byte-ticketing.vercel.app",
+	// sameSite: "none",
 };
 
 export default cookiesOptions;
