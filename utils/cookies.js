59285a3e6d5fb67cf485693e47b572dc03cb909a
Irfandi Iqbal Abimanyu <irfandiabimanyu@gmail.com>
2022-08-23 06:17:29 +0700

    Update cookies.js

--- utils/cookies.js
+++ utils/cookies.js
@@ -3,6 +3,7 @@ import parse from "parse-duration";
 const cookiesOptions = {
 	maxAge: parse(process.env.NEXT_PUBLIC_COOKIE_EXPIRE) / 1000,
 	httpOnly: true,
+	sameSite: false,
 };
 
 export default cookiesOptions;
